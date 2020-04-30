c657c47845c75734588ecb6fa3b4a1d5c298ee0d
Shailee Shah <59224009+Shailee28@users.noreply.github.com>
2020-04-30 15:11:42 +0530

    Add files via upload

---
+++ list_of_shape_below_the_given_shape.py
@@ -0,0 +1,25 @@
+from conversion_of_shape_coordinates_in_clockwise_direction import clockwise_list
+from checks_if_point_inside_the_shape_or_not import point_inside_the_shape
+from checks_intersection_between_two_shapes import intersection_of_shapes
+
+def formation_of_set_S_vertical(list_bottom, S1, list_shape1):  # get list of shapes which are already placed below of given shape
+    list_shape3 = []
+    for g in range(len(list_bottom)):
+        if g > 2:
+            list_shape3.append(list_bottom[g])
+    list_shape3.append(list_bottom[0])
+    list_shape3 = clockwise_list(list_shape3)
+    S = []
+    for i in range(len(S1)):
+        S2 = []
+        if intersection_of_shapes(list_bottom, S1[i]) == True and intersection_of_shapes(list_shape1, S1[i]) == False:
+            if S1[i] != list_shape1:
+                S.append(S1[i])
+        for j in range(len(S1[i])):
+            if point_inside_the_shape(list_bottom, S1[i][j][0], S1[i][j][1]) == True and point_inside_the_shape(
+                    list_shape1, S1[i][j][0], S1[i][j][1]) == False:
+                S3 = S1[i][j][0], S1[i][j][1]
+                S2.append(S3)
+            if S2 == S1[i]:
+                S.append(S1[i])
+    return S, list_shape3
